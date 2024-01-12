b16f5c17ee09cf56e776861d176ad7704f9bd634
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-01-12 00:51:20 -0300

    Tornando genericos filas push e pull e tarefas cron

---
+++ src/main/java/com/ccp/topic/consumer/pubsub/pull/CcpMessageReceiver.java
@@ -0,0 +1,52 @@
+package com.ccp.topic.consumer.pubsub.pull;
+
+import java.util.function.Function;
+
+import com.ccp.constantes.CcpConstants;
+import com.ccp.decorators.CcpJsonRepresentation;
+import com.ccp.especifications.db.utils.CcpEntity;
+import com.ccp.exceptions.process.CcpAsyncProcess;
+import com.google.cloud.pubsub.v1.AckReplyConsumer;
+import com.google.cloud.pubsub.v1.MessageReceiver;
+import com.google.protobuf.ByteString;
+import com.google.pubsub.v1.PubsubMessage;
+
+public class CcpMessageReceiver implements MessageReceiver {
+	
+	private final Function<CcpJsonRepresentation, CcpJsonRepresentation> notifyError ;
+
+	private final CcpEntity asyncTask;
+	
+	public final String name;
+	
+	public CcpMessageReceiver(Function<CcpJsonRepresentation, CcpJsonRepresentation> notifyError,
+			 CcpEntity asyncTask,
+			String name) {
+		this.notifyError = notifyError;
+		this.asyncTask = asyncTask;
+		this.name = name;
+	}
+
+	public void receiveMessage(PubsubMessage message, AckReplyConsumer consumer) {
+		try {
+			ByteString data = message.getData();
+			String receivedMessage = data.toStringUtf8();
+			CcpJsonRepresentation mdMessage = new CcpJsonRepresentation(receivedMessage);
+			try {
+				Function<CcpJsonRepresentation, CcpJsonRepresentation> task = msg -> CcpAsyncProcess.executeProcess(this.name, msg, this.asyncTask);
+				task.apply(mdMessage);
+			} catch (Throwable e) {
+				CcpJsonRepresentation put = CcpConstants.EMPTY_JSON.put("topic", this.name).put("values", mdMessage);
+				throw new RuntimeException(put.asPrettyJson(), e);
+			}
+			consumer.ack();
+		} catch (Throwable e) {
+			CcpJsonRepresentation values = new CcpJsonRepresentation(e);
+			CcpJsonRepresentation execute = this.notifyError.apply(values.renameKey("message", "msg"));
+			this.notifyError.apply(execute);
+			consumer.nack();
+		}
+		
+	}
+
+}
