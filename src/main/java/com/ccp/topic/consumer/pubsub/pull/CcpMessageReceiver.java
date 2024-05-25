22fae8cc582366d9977b4544c9b7f4ffb4f2e121
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-05-25 15:57:05 -0300

    desacoplamento entre sigla 'CCP' e elasticsearch, busca do 'union all ids' considerando registros com prazo de validade (disposable records)

--- src/main/java/com/ccp/topic/consumer/pubsub/pull/CcpMessageReceiver.java
+++ src/main/java/com/ccp/topic/consumer/pubsub/pull/CcpMessageReceiver.java
@@ -5,18 +5,17 @@ import java.util.function.Function;
 import com.ccp.constantes.CcpConstants;
 import com.ccp.decorators.CcpJsonRepresentation;
 import com.ccp.especifications.db.utils.CcpEntity;
-import com.ccp.exceptions.process.CcpAsyncTask;
 import com.google.cloud.pubsub.v1.AckReplyConsumer;
 import com.google.cloud.pubsub.v1.MessageReceiver;
 import com.google.protobuf.ByteString;
 import com.google.pubsub.v1.PubsubMessage;
 
 public class CcpMessageReceiver implements MessageReceiver {
-	private final Function<CcpJsonRepresentation, CcpJsonRepresentation> jnAsyncBusinessNotifyError;
+	protected final Function<CcpJsonRepresentation, CcpJsonRepresentation> jnAsyncBusinessNotifyError;
 	
 	private final Function<CcpJsonRepresentation, CcpJsonRepresentation> notifyError ;
 
-	private final CcpEntity asyncTask;
+	protected final CcpEntity asyncTask;
 	
 	public final String name;
 
@@ -36,8 +35,18 @@ public class CcpMessageReceiver implements MessageReceiver {
 			String receivedMessage = data.toStringUtf8();
 			CcpJsonRepresentation mdMessage = new CcpJsonRepresentation(receivedMessage);
 			try {
-				Function<CcpJsonRepresentation, CcpJsonRepresentation> task = msg -> CcpAsyncTask.executeProcess(this.name, msg, this.asyncTask, this.jnAsyncBusinessNotifyError);
-				task.apply(mdMessage);
+/*				Function<CcpJsonRepresentation, CcpJsonRepresentation> task = msg -> 
+ * 					CcpAsyncTask.executeProcess(this.name, msg, 
+ * 					this.asyncTask, this.jnAsyncBusinessNotifyError);
+*/
+//				Function<CcpJsonRepresentation, CcpJsonRepresentation> task = msg -> 			
+//				JnAsyncMensageriaSender.INSTANCE.executeProcesss(
+//						this.asyncTask, 
+//						this.name, 
+//						msg, 
+//						this.jnAsyncBusinessNotifyError
+//						);
+//				task.apply(mdMessage);
 			} catch (Throwable e) {
 				CcpJsonRepresentation put = CcpConstants.EMPTY_JSON.put("topic", this.name).put("values", mdMessage);
 				throw new RuntimeException(put.asPrettyJson(), e);
